7e81de80a51bf7683561538704723b8fd991b14d
atuonufure <atuonufure@gmail.com>
2022-07-29 12:33:12 +0200

    Update

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ def get_names(path):
 def convert_dcm_to_png(path_with_filename):
     im = pydicom.dcmread(path_with_filename)
     im = im.pixel_array.astype(float)
-    rescaled_image = (np.maximum(im, 0) / im.max()) * 255
-    final_image = np.uint8(rescaled_image)
+    rescaled_image = (np.maximum(im, 0) / im.max()) * 255  # float pixels
+    final_image = np.uint8(rescaled_image)  # integers pixels
     final_image = Img.fromarray(final_image)
     return final_image
 
@@ -31,19 +31,27 @@ def create_folder(path):
 
 
 async def handle(request):
-    root_path = "../ipfs-node-js/"
+    patientId = request.match_info.get("patientId", "empty")
+    root_path = "../ipfs-nodejs/"
     dicom_folder = "dicom-files"
     png_folder = "png-files"
-    names = get_names(root_path + dicom_folder)
-    create_folder(root_path + png_folder)
+    names = get_names(f"{root_path}{dicom_folder}")
+    create_folder(f"{root_path}{png_folder}")
+    create_folder(f"{root_path}/{png_folder}/{patientId}")
     for name in names:
-        image = convert_dcm_to_png(root_path + dicom_folder + "/" + name)
-        image.save(root_path + png_folder + "/" + name + ".png")
+        image = convert_dcm_to_png(f"{root_path}{dicom_folder}/{name}")
+        image.save(f"{root_path}{png_folder}/{patientId}/{name}.png")
+        print(f"{root_path}{png_folder}/{patientId}/{name}.png saved")
+    for name in names:
+        print("start remove")
+        os.remove(f"{root_path}{dicom_folder}/{name}")
+        print(f"{root_path}{dicom_folder}/{name} removed")
+    print("ok")
     return web.Response(text="ok")
 
 
 app = web.Application()
-app.add_routes([web.get("/", handle), web.get("/{name}", handle)])
+app.add_routes([web.get("/", handle), web.get("/{patientId}", handle)])
 
 if __name__ == "__main__":
     web.run_app(app, port=8089)
