31ef245f7995a98ccb338891dbc0379f49b60cfc
atuonufure <atuonufure@gmail.com>
2022-08-08 14:16:22 +0200

    Renaming variables

--- main.py
+++ main.py
@@ -17,13 +17,13 @@ async def download(download_url):
     return image
 
 
-def convert_dcm_to_png(file):
-    im = pydicom.dcmread(BytesIO(file))
+def convert_dcm_to_png(dcm_image):
+    im = pydicom.dcmread(BytesIO(dcm_image))
     im = im.pixel_array.astype(float)
     rescaled_image = (np.maximum(im, 0) / im.max()) * 255  # float pixels
-    final_image = np.uint8(rescaled_image)  # integers pixels
-    final_image = Img.fromarray(final_image)
-    return final_image
+    png_image = np.uint8(rescaled_image)  # integers pixels
+    png_image = Img.fromarray(png_image)
+    return png_image
 
 
 async def handle(request):
@@ -41,6 +41,7 @@ async def handle(request):
 
 app = web.Application()
 app.add_routes([web.get("/get-png-image", handle)])
+
 
 if __name__ == "__main__":
     web.run_app(app, port=8080)
